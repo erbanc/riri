072b5ace426edd24a3621d86cd6b845ad2b910e7
Erwan Bancourt <erwan.bancourt@gmail.com>
2020-09-19 14:54:13 +0200

    chore: ajoute le squelette spring

---
+++ src/main/java/com/erbanc/riri/RiriApplication.java
@@ -0,0 +1,13 @@
+package com.erbanc.riri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiriApplication.class, args);
+	}
+
+}
